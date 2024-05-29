992abc4b234830baeb39bd1232d5fc7d84cb874b
Ari Gaskins <arigaskins.tech@gmail.com>
2024-05-28 22:38:01 -0400

    add function stubs

---
+++ file_organizer.py
@@ -0,0 +1,54 @@
+import os
+
+my_dirs = ['Projects', 'Hackathons', 'Documents', 'Desktop', 'Fonts', 'Templates',
+             'Hacking', 'Powerhouse_Vault', 'Obsidian_Plugin_Sandbox', 'Pictures',
+             'Music', 'Downloads', 'Arduino', 'anaconda3', 'Videos', 'Backups',
+             'Research', 'College']
+
+# TODO: list comprehension of hidden files
+off_limits_dirs = []
+
+# TODO: mkdir from dirs list
+def create_required_dirs():
+    pass
+
+
+# TODO: mv dirs to Research based on criteria
+def research_dir_funnel():
+    pass
+
+
+# TODO: mv dirs to College based on criteria
+def college_dir_funnel():
+    pass
+
+
+# TODO: mv dirs to Hackathon based on criteria
+def hackathon_dir_funnel():
+    pass
+
+
+# TODO: mv dirs to Projects and Research based on criteria
+def projects_research_dirs_funnel():
+    pass
+
+
+# TODO: mv files to Backups based on criteria
+def backups_dir_funnel():
+    pass
+
+# TODO: clean up Downloads dir based on criteria
+def cleanup_downloads_dir():
+    pass
+
+
+def mv_doc_files_to_documents():
+    pass
+
+
+def mv_pic_files_to_pictures():
+    pass
+
+
+def del_zip_files():
+    pass
