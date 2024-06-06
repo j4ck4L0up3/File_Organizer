2d7ee24cd1aead853715ae2751082983e3daed17
Ari Gaskins <arigaskins.tech@gmail.com>
2024-06-05 22:34:57 -0400

    add logging, get basic structure of directory funnels, create test file

--- file_organizer.py
+++ file_organizer.py
@@ -1,36 +1,163 @@
 import os
+import shutil
+import logging
+from pathlib import Path
 
 my_dirs = ['Projects', 'Hackathons', 'Documents', 'Desktop', 'Fonts', 'Templates',
              'Hacking', 'Powerhouse_Vault', 'Obsidian_Plugin_Sandbox', 'Pictures',
              'Music', 'Downloads', 'Arduino', 'anaconda3', 'Videos', 'Backups',
              'Research', 'College']
 
-# TODO: list comprehension of hidden files
-off_limits_dirs = []
+home_path = Path.home()
+
+logging.basicConfig(
+    filename='logging.log', level=logging.DEBUG,  
+    format=' %(asctime) - %(levelname) - %(message)'
+)
+
+# get list of non-hidden directories
+def get_non_hidden_dirs(directory=None):
+    if directory is None:
+        return [], []
+    
+    approved_items = [item for item in os.listdir(directory) if not item.startswith(".")]
+    approved_dirs = [item for item in approved_items if os.path.isdir(Path(directory, item))]
+    return approved_dirs
+
 
 # TODO: mkdir from dirs list
-def create_required_dirs():
-    pass
+def create_required_dirs(dirs, home):
+    try:
+        # mkdir each dir in list
+        for directory in dirs:
+            if Path.exists(directory):
+                continue
+            else:
+                os.makedirs(home / directory)
+                logging.debug(f'{directory} has been added!')
+    
+    except OSError as ose:
+        logging.error(f'OSError in create_required_dirs: {ose}')
+
+    except Exception as e:
+        logging.error(f'Error in create_required_dirs: {e}')
 
 
 # TODO: mv dirs to Research based on criteria
-def research_dir_funnel():
-    pass
+def research_dir_funnel(home):
+    try:
+        approved_home_dirs = get_non_hidden_dirs(home)
+        for directory in approved_home_dirs:
+            if directory == 'Research':
+                continue
+
+            for item in os.listdir(directory):
+                has_git_file = Path.exists(home / directory / item /'.git')
+                is_research = item.startswith('Learn') or item.startswith('Study') or item.startswith('Test')
+                is_research = is_research and has_git_file
+                if not is_research:
+                    continue
+                if is_research:
+                    source_path = home / directory / item
+                    destination_path = home / 'Research'
+                    logging.info(f'Source path in research_dir_funnel: {source_path}')
+                    logging.info(f'Destination path in research_dir_funnel: {destination_path}')
+                    # TODO: test before moving anything
+                    # moved_dir_path = shutil.move(source_path, destination_path)
+                    # logging.info(f'Directory was move to {moved_dir_path}')
+    except OSError as ose:
+        logging.error(f'OSError in research_dir_funnel: {ose}')
+    
+    except Exception as e:
+        logging.error(f'Error in research_dir_funnel: {e}')
 
 
 # TODO: mv dirs to College based on criteria
-def college_dir_funnel():
-    pass
+def college_dir_funnel(home):
+    try:
+        approved_home_dirs = get_non_hidden_dirs(home)
+        for directory in approved_home_dirs:
+            if directory == 'College':
+                continue
+            
+            # TODO: filter out hidden files/directories
+            for item in os.listdir(directory):
+                is_college = item.startswith('CS') 
+                if not is_college:
+                    continue
+                if is_college:
+                    source_path = home / directory / item
+                    destination_path = home / 'College'
+                    logging.info(f'Source path in college_dir_funnel: {source_path}')
+                    logging.info(f'Destination path in college_dir_funnel: {destination_path}')
+                    # TODO: test before moving anything
+                    # moved_dir_path = shutil.move(source_path, destination_path)
+                    # logging.info(f'Directory was move to {moved_dir_path}')
+    except OSError as ose:
+        logging.error(f'OSError in college_dir_funnel: {ose}')
+    
+    except Exception as e:
+        logging.error(f'Error in college_dir_funnel: {e}')
+
 
 
 # TODO: mv dirs to Hackathon based on criteria
-def hackathon_dir_funnel():
-    pass
+def hackathon_dir_funnel(home):
+    try:
+        approved_home_dirs = get_non_hidden_dirs(home)
+        for directory in approved_home_dirs:
+            if directory == 'Hackathon':
+                continue
+
+            # TODO: filter out hidden files/directories
+            for item in os.listdir(directory):
+                has_git_file = Path.exists(home / directory / item /'.git')
+                is_hackathon = item.startswith('Hackathon') and has_git_file
+                if not is_hackathon:
+                    continue
+                if is_hackathon:
+                    source_path = home / directory / item
+                    destination_path = home / 'Hackathon'
+                    logging.info(f'Source path in hackathon_dir_funnel: {source_path}')
+                    logging.info(f'Destination path in hackathon_dir_funnel: {destination_path}')
+                    # TODO: test before moving anything
+                    # moved_dir_path = shutil.move(source_path, destination_path)
+                    # logging.info(f'Directory was move to {moved_dir_path}')
+    except OSError as ose:
+        logging.error(f'OSError in hackthon_dir_funnel: {ose}')
+    
+    except Exception as e:
+        logging.error(f'Error in hackathon_dir_funnel: {e}')
 
 
-# TODO: mv dirs to Projects and Research based on criteria
-def projects_research_dirs_funnel():
-    pass
+# TODO: mv dirs to Projects based on criteria
+def projects_dirs_funnel(home):
+    try:
+        approved_home_dirs = get_non_hidden_dirs(home)
+        for directory in approved_home_dirs:
+            if directory == 'Projects':
+                continue
+            if directory == 'Research':
+                continue
+
+            # TODO: filter out hidden files/directories
+            for item in os.listdir(directory):
+                has_git_file = Path.exists(home / directory / item /'.git')
+                if not has_git_file:
+                    continue
+                if has_git_file:
+                    source_path = home / directory / item
+                    destination_path = home / 'Hackathon'
+                    logging.info(f'Source path in hackathon_dir_funnel: {source_path}')
+                    logging.info(f'Destination path in hackathon_dir_funnel: {destination_path}')
+                    # TODO: test before moving anything
+                    # moved_dir_path = shutil.move(source_path, destination_path)
+                    # logging.info(f'Directory was move to {moved_dir_path}')
+    except OSError as ose:
+        logging.error(f'OSError in hackthon_dir_funnel: {ose}')
+    
+    except Exception as e:
+        logging.error(f'Error in hackathon_dir_funnel: {e}')
 
 
 # TODO: mv files to Backups based on criteria
--- test_file_organizer.py
+++ test_file_organizer.py
@@ -1,2 +1,9 @@
 import logging
 import pytest
+
+
+def test_get_hidden_items():
+    pass
+
+def test_create_require_dirs():
+    pass
